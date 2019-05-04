bc6095fe37e3b5a2dbb978cf0bdba9d26826c7b8
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2019-05-04 19:21:13 -0400

    working code

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import psycopg2
 import numpy as np
 import matplotlib.pyplot as plt
-from pyqtgraph.Qt import QtCore, QtGui
-import pyqtgraph.opengl as gl
-import pyqtgraph as pg
-import sys
+from time import sleep
 
 
 def plot_data(data, title):
@@ -17,7 +14,7 @@ def create_X(cur):
     X = []
     speeds = []
     Y = create_y(cur)
-    cur.execute(f'SELECT id_trip, speed from points ORDER BY id_trip limit 1000000;')
+    cur.execute(f'SELECT id_trip, speed from points ORDER BY id_trip limit 100000;')
     trip_no = 0
     for id_trip, speed in cur:
         speeds.append(speed)
@@ -28,11 +25,13 @@ def create_X(cur):
                 # print(mode)
                 if len(speeds) > 10:
                     print('ID_X', id_trip, ' = ', 'ID_Y', mode)
-                    # print('x_id_trip', id_trip)
-                    # print('y_val', Y[id_trip])
-                    # print('------------------')
-                    # print(speeds)
-                    # plot_data(speeds, mode)
+                    print('x_id_trip', id_trip)
+                    print('y_val', Y[id_trip])
+                    print('------------------')
+                    print(speeds)
+                    if mode == 'À pied':
+                        sleep(0.5)
+                        plot_data(speeds, mode)
                     X.append(speeds)
             except:
                 'no mode associated'
@@ -49,57 +48,13 @@ def create_y(cur):
     for id_trip, mode in y_cur:
         if mode != None:
             if not ('/' in mode or ',' in mode):
-                Y[id_trip] = id_trip
+                Y[id_trip] = mode
     print(Y)
     return Y
 
 
-class Visualizer:
-    def __init__(self):
-        self.traces = {}
-        self.app = QtGui.QApplication(sys.argv)
-        self.w = gl.GLViewWidget()
-        self.w.opts['distance'] = 40
-        self.w.setGeometry(0, 110, 1920, 1080)
-        self.w.show()
 
-        self.phase = 0
-        self.lines = 100
-        self.points = 100
-        self.y = np.linspace(-10, 10, self.lines)
-        self.x = np.linspace(-10, 10, self.points)
 
-        for i, line in enumerate(self.y):
-            self.traces[i] = gl.GLLinePlotItem()
-            self.w.addItem(self.traces[i])
-
-    def set_plotdata(self, name, points, color, width):
-        self.traces[name].setData(pos=points, color=color, width=width)
-
-    def update(self):
-        for i, line in enumerate(self.y):
-            y = np.array([line] * self.points)
-
-            amp = 10 / (i + 1)
-            phase = self.phase * (i + 1) - 10
-            freq = self.x * (i + 1) / 10
-
-            sine = amp * np.sin(freq - phase)
-            pts = np.vstack([self.x, y, sine]).transpose()
-
-            print('i', i, 'self.lines', self.lines)
-            print(pg.glColor((i, self.lines*4)))
-            self.set_plotdata(
-                name=i, points=pts,
-                color=pg.glColor((i, self.lines * 4)),
-                width=1)
-            self.phase -= .0002
-
-    def animation(self):
-        timer = QtCore.QTimer()
-        timer.timeout.connect(self.update)
-        timer.start(10)
-        QtGui.QApplication.instance().exec_()
 
 
 
