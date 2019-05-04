5737b96831f0864ea1c0fa17d95b7aa517a26036
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2019-05-04 15:01:22 -0400

    Plot data

--- main.py
+++ main.py
@@ -4,36 +4,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 # import pyqtgraph as pg
 
+def plot_data(data, title):
+    plt.plot(list(range(len(data))), data)
+    plt.title(title)
+    plt.show()
+
+
+def create_X(cur):
+    X = []
+    for trip_id in range(100):
+        cur.execute(f'SELECT speed from points WHERE id_trip={trip_id};')
+        speeds = []
+        for speed in cur:
+            speeds.append(*speed)
+        if speeds != []:
+            print('-------------------')
+            print('speeds', speeds)
+            print('TRIP ID', trip_id)
+            y = create_y(cur, id_trip=trip_id)
+            print(y)
+            X.append(speeds)
+            plot_data(speeds, y[1])
+    return X
+
+
+def create_y(cur, id_trip):
+    cur.execute(f'SELECT id_trip, mode from trips WHERE id_trip={id_trip};')
+    for id_trip, mode in cur:
+        return [id_trip, mode]
+
 
 def main():
     conn = psycopg2.connect(
             "dbname='mtltrajet' user='kyle' host='localhost' password='123'")
     cur = conn.cursor()
-    for trip_id in range(100):
-        lats = []
-        lons = []
-        speeds = []
-        cur.execute(f'SELECT latitude, longitude, speed from points WHERE id_trip={trip_id};')
-        for lat, lon, speed in cur:
-            lats.append(lat)
-            lons.append(lon)
-            speeds.append(speed)
-
-        if lats != []:
-            plt.plot(lats, lons)
-            plt.show()
-
-            plt.plot(list(range(len(speeds))), speeds)
+    X = create_X(cur)
+    # y = create_y(cur)
+    # print(X)
 
 
-    # DATABASE_URI = 'postgres://kyle:123@localhost/mtltrajet'
-    # eng = create_engine(DATABASE_URI)
-    #
-    # with eng.connect() as con:
-    #     rs = con.execute('SELECT 5')
-    #     data = rs.fetchone()[0]
-    #     print(f'Data: {data}', data)
-    # df = pd.read_sql(engine.)
+
+
 
 if __name__ == '__main__':
     main()
+
+
+
+
